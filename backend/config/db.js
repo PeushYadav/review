289bc86b1f5a568618f435763f46e0670e25687c
PeushYadav <peushyadav07@gmail.com>
2025-07-05 17:55:45 +0530

    some changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
 
 const connectDB = async () => {
   try {
+    console.log('DEBUG: MONGO_URI =', process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB connected');
   } catch (err) {
